6a383a838927d62718740b14a1b9bdcdbdfa3787
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2023-05-26 03:03:44 -0700

    problem#1

---
+++ problem#1/README.md
@@ -0,0 +1,32 @@
+### Problem #1
+
+If we list all the natural numbers below  that are multiples of  or , we get  and . The sum of these multiples is .
+
+Find the sum of all the multiples of  or  below .
+
+Input Format
+
+First line contains  that denotes the number of test cases. This is followed by  lines, each containing an integer, .
+
+Constraints
+
+Output Format
+
+For each test case, print an integer that denotes the sum of all the multiples of  or  below .
+
+Sample Input
+
+```
+2
+10
+100
+```
+Sample Output
+```
+23
+2318
+```
+
+Explanation
+
+For , if we list all the natural numbers below  10 that are multiples of 3 or 5, we get  3,5,6 and 9. The sum of these multiples is 23.
--- problem#1/main.go
+++ problem#1/main.go
@@ -1,33 +1,3 @@
-//  Problem #1
-// If we list all the natural numbers below  that are multiples of  or , we get  and . The sum of these multiples is .
-
-// Find the sum of all the multiples of  or  below .
-
-// Input Format
-
-// First line contains  that denotes the number of test cases. This is followed by  lines, each containing an integer, .
-
-// Constraints
-
-// Output Format
-
-// For each test case, print an integer that denotes the sum of all the multiples of  or  below .
-
-// Sample Input 0
-
-// 2
-// 10
-// 100
-// Sample Output 0
-
-// 23
-// 2318
-// Explanation 0
-
-// For , if we list all the natural numbers below  that are multiples of  or , we get  and . The sum of these multiples is .
-
-// Similarly for , we get .
-
 package main
 
 import (
